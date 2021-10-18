97587b266b8c860b9c1040b4a7871c61132a40ed
jiyl <jiyl@coscon.com>
2021-10-18 16:34:23 +0800

    20210728

---
+++ learn_fastapi.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    return item
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run("learn_fastapi:app", host="0.0.0.0", port=5555)
